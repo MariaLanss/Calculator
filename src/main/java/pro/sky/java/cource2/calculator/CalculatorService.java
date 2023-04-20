9d2ac5470d73565a55a9fb64369a2bc6b9dedfca
MariaLanss <maruska0990@gmail.com>
2023-04-20 13:27:20 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/cource2/calculator/CalculatorService.java
@@ -0,0 +1,8 @@
+package pro.sky.java.cource2.calculator;
+
+public interface CalculatorService {
+    String getCalculateAddition(int number1, int number2);
+    String getCalculateSubtraction(int number1, int number2);
+    String getCalculateMultiplication(int number1, int number2);
+    String getCalculateDivision(int number1, int number2);
+}
